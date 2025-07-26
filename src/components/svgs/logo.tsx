ef4b37edc90fe47a1ae30800e0d5157d2726ae53
Mr-Singh-69 <navdeep13dps@gmail.com>
2025-07-26 16:26:52 +0530

    updated logo

--- src/components/svgs/logo.tsx
+++ src/components/svgs/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Logo() {
   return (
     <Image
-      src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/4070647f8d8a56a5ecd1490f04d519ccf7c251ec_plateulogo.png"
+      src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/90158b0c273bcbef6a4d173682eb90293a3dd5bc_group_5.png"
         width={200}
         height={50}
       alt="Plateu Logo"
